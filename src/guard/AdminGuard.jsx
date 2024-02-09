d8212a9cbd6988783b18b73c65fdc8b83189415d
Sarah Levi <sarah75373@gmail.com>
2024-02-09 10:36:22 +0200

    footer

---
+++ src/guard/AdminGuard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function AdminGuard() {
+  return (
+    <Box>
+      <Typography variant="h1" color="initial">
+        AdminGuard
+      </Typography>
+    </Box>
+  );
+}
+
+export default AdminGuard;
